be371b24e617934cffc7348003340f014d19e8e2
Agata Magier <magier.agata@gmail.com>
2018-10-28 13:04:40 +0000

    reconfig

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/magbucket_new',
 
     /**
      * Source Maps
